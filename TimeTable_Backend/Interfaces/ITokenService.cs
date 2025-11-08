d098983d3dd5b95a5181672edef0d118cb822ad9
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-11-08 13:40:24 +0700

    Login

---
+++ TimeTable_Backend/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTable_Backend.models;
+
+namespace TimeTable_Backend.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(User user);
+    }
+}
